8e934071c6d9fab52da0e1d9211358b571482759
Sortient <osortwell@gmail.com>
2025-06-08 17:37:57 +0100

    Progress so far.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header/header.component";
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostCreateComponent, HeaderComponent, PostListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mean-stack';
+  storedPosts: any[] = [];
+
+  onPostAdded(post: any) {
+    this.storedPosts.push(post);
+  }
 }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-post-list',
+  imports: [MatExpansionModule],
+  templateUrl: './post-list.component.html',
+  styleUrl: './post-list.component.scss'
+})
+export class PostListComponent {
+  @Input() posts: any = [];
+}
